8f4239bac64db32a997bafb9e8b0796012ba6315
Antony Hutchison <antonyh@gmail.com>
2012-09-07 23:39:47 +0100

    added License & Readme

--- 1.sh
+++
@@ -1,5 +0,0 @@
-mvn archetype:generate \
--DarchetypeRepository=http://maven.onehippo.com/maven2 \
--DarchetypeGroupId=org.onehippo.cms7 \
--DarchetypeArtifactId=hippo-archetype-website \
--DarchetypeVersion=1.05.05
