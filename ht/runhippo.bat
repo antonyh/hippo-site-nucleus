fe6ba47286d7fd2ce949649a82cd14c40a1ca960
Antony Hutchison <antonyh@gmail.com>
2012-11-08 22:23:18 +0000

    clean up runhippo.bat
    remove jRebel.

--- ht/runhippo.bat
+++ ht/runhippo.bat
@@ -4,9 +4,9 @@ echo Exit Code = %ERRORLEVEL%
 if not "%ERRORLEVEL%" == "0" exit /b
 
 
-call mvn install -Djrebel
+call mvn install 
 echo Exit Code = %ERRORLEVEL%
 if not "%ERRORLEVEL%" == "0" exit /b
 
   
-call mvn -P cargo.run -Drepo.path=o:/myhipporepo -Djrebel
+call mvn -P cargo.run -Drepo.path=t:/hippo/repo 
